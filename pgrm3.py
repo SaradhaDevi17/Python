921d4c4554ba19ae8aacb0d52f3d5829b6eca438
SaradhaDevi17 <53566769+SaradhaDevi17@users.noreply.github.com>
2019-08-02 14:27:04 +0530

    Add files via upload

---
+++ pgrm3.py
@@ -0,0 +1,11 @@
+year=int(input("enter the year"))
+if year%4==0:
+    if year%100==0:
+        if year%400==0:
+            print("leap yr")
+        else:
+            print("not a leap yr")
+    else:
+        print("leap yr")
+else:
+    print("not a leap yr")
