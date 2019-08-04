3d8b318a8b0e79de5f1e5253e21515deda7072ef
SaradhaDevi17 <53566769+SaradhaDevi17@users.noreply.github.com>
2019-08-04 09:43:40 +0530

    Add files via upload

---
+++ addition2.py
@@ -0,0 +1,7 @@
+n1=int(input("enter the first no"))
+n2=int(input("enter the second no"))
+sum=n1+n2
+if sum<20:
+    print("sum is 20")
+else:
+    print("sum",sum)
---
+++ addorsub1.py
@@ -0,0 +1,13 @@
+n1=int(input("enter the first value"))
+n2=int(input("enter the second value"))
+if n1==n2:
+    print("true")
+else:
+    print("elsepart")
+    s=n1+n2
+    d=n1-n2
+    if s==d==5:
+        print("true")
+    else:
+        print("false")
+        
