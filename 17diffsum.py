69ce5f4cc5e6d8298c92582c025fe06d3bbc9dd5
SaradhaDevi17 <53566769+SaradhaDevi17@users.noreply.github.com>
2019-08-04 08:44:34 +0530

    Add files via upload

---
+++ 17diffsum.py
@@ -0,0 +1,8 @@
+n=int(input("Enter the number"))
+if n>17:
+    ans=(n-17)*2
+    print(ans)
+else:
+    ans=n-17
+    print(ans)
+    
---
+++ clrprgm.py
@@ -0,0 +1,10 @@
+color=['red','green','white','black']
+for x in color:
+    if x=='red':
+        print(x)
+    if x=='green':
+        if x=='white':
+            continue
+print(x)
+    
+        
