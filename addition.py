ff389090dd460a7d4f5e8d716ec310165fc37eb1
SaradhaDevi17 <53566769+SaradhaDevi17@users.noreply.github.com>
2019-08-04 08:55:45 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,11 @@
+n1=int(input("enter the first no"))
+n2=int(input("enter the second no"))
+n3=int(input("enter the third no"))
+if n1==n2==n3:
+    sum=n1+n2+n3
+    sum= sum*3
+    print("sum",sum)
+else:
+    sum=n1+n2+n3
+    print("sum",sum)
+    
