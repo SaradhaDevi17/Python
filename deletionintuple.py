979808d7e76b479c267d1216ff425971bf9e8ab1
SaradhaDevi17 <53566769+SaradhaDevi17@users.noreply.github.com>
2019-08-05 22:17:08 -0700

    Add files via upload

---
+++ deletionintuple.py
@@ -0,0 +1,4 @@
+tup=(1,2,3,4,5,6)
+print("before deletions",tup)
+tup=tup[0:2]+tup[3:]
+print("after deletion",tup)
---
+++ tupletostring.py
@@ -0,0 +1,3 @@
+tup=('s','a','r','a')
+string=''.join(tup)
+print("the string is",string)
